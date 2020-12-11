f81093bc09ca89797e3476a8c00740e4f1ef13be
mgarciafedor87 <71287575+mgarciafedor87@users.noreply.github.com>
2020-12-11 00:49:19 -0500

    My Final
    Hopefully everything is done!

---
+++ myscripts.js
@@ -0,0 +1,149 @@
+function assignment()
+{
+  var image = document.getElementById("1920-img_Change");
+  image.srcset="images/notes-800.png"
+  var image2 = document.getElementById("1522-img_Change");
+  image2.srcset="images/notes-600.png"
+  var text = document.getElementById("event-P_1");
+  text.innerHTML = "Post your code via a shared link in the critique space on WebEx teams"
+  var text2 = document.getElementById("event-P_2");
+  text2.innerHTML = "Critique your fellow students code; check for any validation errors, review for quality"
+  var header = document.getElementById("event-h");
+  header.innerHTML = "Week 15"
+  var time = document.getElementById("event-time");
+  time.innerHTML = "11:59pm"
+  document.getElementById("to_bookclub").style.opacity = "0";
+  document.getElementById("to_ladies").style.opacity = "0";
+}
+function girlsnight()
+{
+  var image = document.getElementById("1920-img_Change");
+  image.srcset="images/ladies-800.png"
+  var image2 = document.getElementById("1522-img_Change");
+  image2.srcset="images/ladies-600.png"
+  var text = document.getElementById("event-P_1");
+  text.innerHTML = "Ladies over 21, the your night will begin at 7pm."
+  var text2 = document.getElementById("event-P_2");
+  text2.innerHTML = "Look forward to 50% off of drink specials and 75% off any wine of your choice."
+  var header = document.getElementById("event-h");
+  header.innerHTML = "Girl's Night"
+  var time = document.getElementById("event-time");
+  time.innerHTML = "7:00pm"
+  document.getElementById("to_bookclub").style.opacity = "1";
+  document.getElementById("to_ladies").style.opacity = "1";
+  document.getElementById("to_bookclub").style.color = "white";
+  document.getElementById("to_ladies").style.color = "gray";
+}
+function boysnight()
+{
+  var image = document.getElementById("1920-img_Change");
+  image.srcset="images/gentlemen-800.png"
+  var image2 = document.getElementById("1522-img_Change");
+  image2.srcset="images/gentlemen-600.png"
+  var text = document.getElementById("event-P_1");
+  text.innerHTML = "Gentlemen over 21, you get your own night and it will begin at 7pm."
+  var text2 = document.getElementById("event-P_2");
+  text2.innerHTML = "Look forward to 50% off of drink specials and 75% off any beer of your choice."
+  var header = document.getElementById("event-h");
+  header.innerHTML = "Boy's Night"
+  var time = document.getElementById("event-time");
+  time.innerHTML = "7:00pm"
+  document.getElementById("to_bookclub").style.opacity = "0";
+  document.getElementById("to_ladies").style.opacity = "0";
+}
+function spanishclub()
+{
+  var image = document.getElementById("1920-img_Change");
+  image.srcset="images/spanishclub-800.png"
+  var image2 = document.getElementById("1522-img_Change");
+  image2.srcset="images/spanishclub-600.png"
+  var text = document.getElementById("event-P_1");
+  text.innerHTML = "Quieres aprender espa&ntilde;ol?"
+  var text2 = document.getElementById("event-P_2");
+  text2.innerHTML = "We meet the first monday every month. Look forward to good conversation completely in Spanish."
+  var header = document.getElementById("event-h");
+  header.innerHTML = "Spanish Club"
+  var time = document.getElementById("event-time");
+  time.innerHTML = "4:30pm"
+  document.getElementById("to_bookclub").style.opacity = "0";
+  document.getElementById("to_ladies").style.opacity = "0";
+}
+function bookclub()
+{
+  var image = document.getElementById("1920-img_Change");
+  image.srcset="images/bookclub-800.png"
+  var image2 = document.getElementById("1522-img_Change");
+  image2.srcset="images/bookclub-600.png"
+  var text = document.getElementById("event-P_1");
+  text.innerHTML = "Interested in participating in a deep literary conversation accompanied by good wine?"
+  var text2 = document.getElementById("event-P_2");
+  text2.innerHTML = "Come join us! Feel free to bring your books suggestions with you."
+  var header = document.getElementById("event-h");
+  header.innerHTML = "Book Club"
+  var time = document.getElementById("event-time");
+  time.innerHTML = "6:00pm"
+  document.getElementById("to_bookclub").style.opacity = "1";
+  document.getElementById("to_ladies").style.opacity = "1";
+  document.getElementById("to_bookclub").style.color = "gray";
+  document.getElementById("to_ladies").style.color = "white";
+}
+function third()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "December 3, 2020"
+}
+function fourth()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "December 4, 2020"
+}
+function sixth()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "December 6, 2020"
+}
+function seventh()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "December 7, 2020"
+}
+function tenth()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "December 10, 2020"
+}
+function eleventh()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "December 11, 2020"
+}
+function seventeenth()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "December 17, 2020"
+}
+function eighteenth()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "December 18, 2020"
+}
+function twentyfourth()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "December 24, 2020"
+}
+function twentyfifth()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "December 25, 2020"
+}
+function thirtyfirst()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "December 31, 2020"
+}
+function first()
+{
+  var datetext = document.getElementById("change_date");
+  datetext.innerHTML = "January 1, 2021"
+}
